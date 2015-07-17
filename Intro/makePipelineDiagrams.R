1b566b0b468bb32b7ce0cbfab4f4c7684a56fac7
Benjamin Chan <chanb@ohsu.edu>
2015-07-17 09:56:24 -0700

    Move Intro presentation to its own folder

--- Intro/makePipelineDiagrams.R
+++ Intro/makePipelineDiagrams.R
@@ -26,8 +26,8 @@ scriptNodes0 <- create_nodes(nodes=LETTERS[1:length(scriptLabels0)],
 productEdges0 <- create_edges(from=c("a", "b", "c", "c", "d", "e"),
                               to  =c("b", "c", "d", "e", "f", "f"),
                               color="black")
-scriptEdges0 <- create_edges(from=c("A", "B", "C", "C", "A", "B"),
-                             to  =c("b", "c", "d", "e", "B", "C"),
+scriptEdges0 <- create_edges(from=c("A", "B", "C", "C"),
+                             to  =c("b", "c", "d", "e"),
                              color="black")
 
 productNodes1 <- create_nodes(nodes=letters[length(productLabels0) + 1],
@@ -45,8 +45,8 @@ scriptNodes1 <- create_nodes(nodes=LETTERS[length(scriptLabels0) + 1],
 productEdges1 <- create_edges(from=c("g"),
                               to  =c("f"),
                               color="black")
-scriptEdges1 <- create_edges(from=c("D", "D"),
-                             to  =c("A", "f"),
+scriptEdges1 <- create_edges(from=c("D", "D", "D", "D"),
+                             to  =c("A", "B", "C", "f"),
                              color="white")
 
 productNodes2 <- create_nodes(nodes=letters[length(productLabels0) + 1],
@@ -64,21 +64,21 @@ scriptNodes2 <- create_nodes(nodes=LETTERS[length(scriptLabels0) + 1],
 productEdges2 <- create_edges(from=c("g"),
                               to  =c("f"),
                               color="white")
-scriptEdges2 <- create_edges(from=c("D", "D"),
-                             to  =c("A", "f"),
+scriptEdges2 <- create_edges(from=c("D", "D", "D", "D"),
+                             to  =c("A", "B", "C", "f"),
                              color="black")
 
 G1 <- create_graph(nodes_df=combine_nodes(productNodes1, scriptNodes1, productNodes0, scriptNodes0), 
                    edges_df=combine_edges(productEdges1, scriptEdges1, productEdges0, scriptEdges0),
                    node_attrs="fontname=Helvetica", 
                    graph_attrs=c("layout=dot", "rankdir=LR"))
-# render_graph(G1)
+render_graph(G1)
 
 G2 <- create_graph(nodes_df=combine_nodes(productNodes2, scriptNodes2, productNodes0, scriptNodes0), 
                    edges_df=combine_edges(productEdges2, scriptEdges2, productEdges0, scriptEdges0),
                    node_attrs="fontname=Helvetica", 
                    graph_attrs=c("layout=dot", "rankdir=LR"))
-# render_graph(G2)
+render_graph(G2)
 
-cat(render_graph(G1, output="SVG"), file="pipelineAlmostReproducible.svg")
-cat(render_graph(G2, output="SVG"), file="pipelineFullyReproducible.svg")
+# cat(render_graph(G1, output="SVG"), file="pipelineAlmostReproducible.svg")
+# cat(render_graph(G2, output="SVG"), file="pipelineFullyReproducible.svg")
